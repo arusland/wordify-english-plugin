5df44f7af4e24a6ee5fbbc37aeaaf0b7890959d2
Ruslan A <ruslan.absalyamov@gmail.com>
2019-01-03 22:00:06 +0300

    Simple implementation of english tokenizer and lemmatizer

--- src/main/kotlin/net/wordify/plugin/english/WordifyEnglishLemmatizer.kt
+++ src/main/kotlin/net/wordify/plugin/english/WordifyEnglishLemmatizer.kt
@@ -5,7 +5,8 @@ import net.wordify.api.WordifyLemmatizer
 class WordifyEnglishLemmatizer : WordifyLemmatizer {
     override fun getLang(): String = Consts.LANG_CODE
 
-    override fun extract(p0: MutableIterator<String>?): MutableIterator<String> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun extract(tokens: MutableIterator<String>): Iterator<String> {
+        // TODO: add super logic to extract lemma
+        return tokens.asSequence().iterator()
     }
 }
--- src/main/kotlin/net/wordify/plugin/english/WordifyEnglishTokenizer.kt
+++ src/main/kotlin/net/wordify/plugin/english/WordifyEnglishTokenizer.kt
@@ -3,9 +3,28 @@ package net.wordify.plugin.english
 import net.wordify.api.WordifyTokenizer
 
 class WordifyEnglishTokenizer : WordifyTokenizer {
+    private val splitRegexEn = Regex("[^a-z\\d\\-]+")
+
     override fun getLang(): String = Consts.LANG_CODE
 
-    override fun parse(rawText: String): MutableIterator<String> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun parse(rawText: String): Iterator<String> {
+        return rawText.toLowerCase()
+                .split(splitRegexEn)
+                .filter { it.length > 1 }
+                .filter { isNotNumber(it) }
+                .iterator()
+    }
+
+    /**
+     * Some token is represented as word if one contains at least one non-digit character
+     */
+    private inline fun isNotNumber(value: String): Boolean {
+        for (ch in value) {
+            if (!(ch.isDigit() || ch == '-')) {
+                return true
+            }
+        }
+
+        return false
     }
 }
